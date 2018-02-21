9e8f2c68fc16f8dcadb58a6290023016fff8f5d7
gvenezia <g.venezia@outlook.com>
2018-02-21 18:03:33 +0000

    finished functionality, changed the button event for a keypress event so that the results update automatically as the user types, added a display function to set the html content for the wikipedia entries, added css styling to the wikiEntries for better presentation and interactivity, added a title and explanation to the page header

--- index.js
+++ index.js
@@ -8,67 +8,54 @@
         wikiEntries     = [];
     
     // HTML elements
-    let searchBtn       = document.getElementById('search-btn');
-    var searchValue     = '';
+    const a              = document.querySelectorAll('a'),
+        h3               = document.querySelectorAll('h3'),
+        p                = document.querySelectorAll('p'),
+        searchBar        = document.getElementById('search-value'),
+        wikiContainer    = document.getElementById('wiki-container'),
+        wikiReturnElement= document.querySelectorAll('.wiki-return-element');
     
     // =============== On Page Load ================
-    document.addEventListener('DOMContentLoaded', function getLocationAndWeatherInformation(){
+    document.addEventListener('DOMContentLoaded', function DOMLoaded(){
         
-        // Click Event on the Search Button
-        searchBtn.addEventListener('click', function (){
-            
-            // update the searchValue
-            searchValue = document.getElementById('search-value').value;
-            
-            // API call
-            getWikiInfoWithSearchValue();    
-            
-        });
+        // update the search results as the user types
+        document.addEventListener('keyup', getWikiInfoAndSetHTML, false);
         
-    });
+    }, {once: true});
 
 
     // ============== Functions ================
-    function getWikiInfoWithSearchValue(){
-        let xhttp   = new XMLHttpRequest();
-        let url     = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchValue}&titles=${searchValue}&prop=revisions&rvprop=content&format=json&formatversion=2&origin=*`;
+    function getWikiInfoAndSetHTML(){
+        let xhttp = new XMLHttpRequest();
+        let url   = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchBar.value}&prop=revisions&rvprop=content&format=json&formatversion=2&origin=*`;
         
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
+                apiResponse = JSON.parse(this.responseText);
+                
+                wikiEntries = apiResponse.query.search;
               
-              apiResponse = JSON.parse(this.responseText);
-              console.log(apiResponse);
-              
-              wikiEntries = apiResponse.query.search;
-              console.log(wikiEntries);
-              
-              displayWikiEntries();
+                // write API response to HTML 
+                displayWikiEntries();
             } 
         };
         
-        // Get the JSON data from the url with user position
         xhttp.open('GET', url, true);
-          
         xhttp.send();
     };
     
-    
     function displayWikiEntries() {
         for (let i = 0; i < wikiEntries.length; i++) {
-            // create a block
-                // display wikiEntries[i].title;
-                wikiEntries[i].title
-                // display wikiEntries[i].snippet;
+
+            // Reveal the div block
+            wikiReturnElement[i].style.height = `auto`;
+            wikiReturnElement[i].style.opacity = `1`;
             
+            // Set its inner contents
+            a[i + 1].href   = `https://en.wikipedia.org/wiki/${wikiEntries[i].title}`;
+            h3[i].innerHTML = wikiEntries[i].title;
+            p[i + 2].innerHTML  = wikiEntries[i].snippet;
         }
-      
-    }
-    
+    };
     
 })();
-
-// when the user presses the search icon, the input value from the search form is used to query the wiki API
-
-// Assign the titles and basic info for all the returning wiki articles
-
-// Display the title and description in cards, or separate paragraph boxes
